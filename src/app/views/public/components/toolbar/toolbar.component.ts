aaaa19348aac956beda862cb04138fdbdeb166af
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-05-28 13:53:33 +0200

    feat(paragraphe):add paragraphe component

--- src/app/views/public/components/toolbar/toolbar.component.ts
+++ src/app/views/public/components/toolbar/toolbar.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements AfterViewInit {
+  @ViewChild('btnBurger') btnBuger!: ElementRef;
+  @ViewChild('menuMobile') menuMobile!: ElementRef;
+  private isOpen = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {}
+  handleShowMenu() {
+    if (!this.isOpen) {
+      this.renderer.addClass(this.btnBuger.nativeElement, 'animate-menu-in');
+      this.renderer.removeClass(
+        this.btnBuger.nativeElement,
+        'animate-close-menu',
+      );
+      this.renderer.addClass(this.menuMobile.nativeElement, 'is-visible');
+      this.renderer.removeClass(this.menuMobile.nativeElement, 'is-hidden');
+      this.isOpen = !this.isOpen;
+    } else {
+      this.renderer.addClass(this.btnBuger.nativeElement, 'animate-close-menu');
+      this.renderer.addClass(this.menuMobile.nativeElement, 'is-hidden');
+      this.renderer.removeClass(this.btnBuger.nativeElement, 'animate-menu-in');
+      this.renderer.removeClass(this.menuMobile.nativeElement, 'is-visible');
+      this.isOpen = !this.isOpen;
+    }
+  }
 }
