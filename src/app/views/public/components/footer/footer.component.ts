2a4850e28dc7872e601f0c3505a88e6974f491ae
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-05-29 21:11:24 +0200

    update(design):update design form

--- src/app/views/public/components/footer/footer.component.ts
+++ src/app/views/public/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { EmailValidator, FormsModule, NgForm } from '@angular/forms';
+import { Email } from '@shared/types';
+import { emailSchema } from '@shared/schemas';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  @ViewChild('newsLetterForm') newsLetterForm!: NgForm;
 
+  email: Email = '';
+
+  constructor() {}
+
+  ngAfterViewInit(): void {}
+  onSubmit() {
+    emailSchema.parse(!this.newsLetterForm.value);
+    if (this.newsLetterForm.invalid) {
+      return console.error('Error format');
+    }
+    console.log(this.newsLetterForm.value);
+  }
 }
